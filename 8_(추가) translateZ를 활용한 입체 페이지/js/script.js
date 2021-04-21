963fc5de53755fce353b67e48813be09b23d67f3
YM.Park-desktop <ympark4@ls-electric.com>
2021-04-22 08:54:47 +0900

    Update script.js

--- 8_(추가) translateZ를 활용한 입체 페이지/js/script.js
+++ 8_(추가) translateZ를 활용한 입체 페이지/js/script.js
@@ -17,13 +17,16 @@ window.onload = function() {
 function scrollFunc(e) {
     scrollTop = this.scrollY;
 
-    //let per = Math.ceil(scrollTop / (_documentHum - _windowHNum) * 100);
+    //let per = Math.ceil(scrollTop / (_documentHum - _windowHNum) * 100); // 상단 로딩바 부분
     //progressBar.style.width = per + "%";
 
     for(var i=0; i< totalNum ; i++){
-        imageAll[i].style.transform = "perspective(400px) translateZ("+ scrollTop/(5*(totalNum-i)) +"px)";
-        imageAll[i].style.transform = "perspective(400px) translateZ("+ scrollTop/5 +"px)";
-        console.log(scrollTop , scrollTop / (5*(totalNum-i)) );
+        //perspective: 입체감 깊이
+        imageAll[i].style.transform = "perspective(400px) translateZ("+ scrollTop/(5*(totalNum-i)) +"px)"; // 이 수식의 의미: 제일 뒤 이미지가 제일 느리게 움직이고, 제일 앞 이미지가 제일 빠르게 움직여서 입체감을 준다.
+        //imageAll[i].style.transform = "perspective(400px) translateZ("+ scrollTop/5 +"px)";
+
+        //console.log( scrollTop , totalNum, i, totalNum-i );
+        //console.log( scrollTop / (5*(totalNum-i)) );
     }
 
 }
